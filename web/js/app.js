87c4b52ccae3fd2368e94ac675bec51e8b16c882
Anatolijs Tarasovs <57757129+anatolijstarasovs@users.noreply.github.com>
2020-12-15 16:39:42 +0200

    Added fireworks.

--- web/js/app.js
+++ web/js/app.js
@@ -3,6 +3,19 @@ let config = {
     gameover: 2
 };
 const Game = (function() {
+    function startFireworks(Fireworks, container) {
+        const fireworks = new Fireworks(container, {
+          maxRockets: 3, // max # of rockets to spawn
+          rocketSpawnInterval: 150, // millisends to check if new rockets should spawn
+          numParticles: 100, // number of particles to spawn when rocket explodes (+0-10)
+          explosionMinHeight: 0.2, // percentage. min height at which rockets can explode
+          explosionMaxHeight: 0.9, // percentage. max height before a particle is exploded
+          explosionChance: 0.08 // chance in each tick the rocket will explode
+        })
+      
+        fireworks.start()
+    }
+   
     let canvas = [],
         context = [],
         grid = [],
@@ -94,6 +107,7 @@ const Game = (function() {
         status = config.gameover;
         turn = false;
         if (winner) {
+            startFireworks(Fireworks, document.getElementById('game'))
             $("#turn-status")
                 .removeClass("opponent-turn")
                 .removeClass("my-turn")
